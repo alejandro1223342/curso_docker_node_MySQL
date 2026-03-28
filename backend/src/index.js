c1afae3727b97980899872b2038b660c62ed8693
alejandro1223342 <alejomuoss@gmail.com>
2026-03-28 17:36:15 -0500

    feat/configuracion de multiples variables de entorno

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,20 @@
-const express = require ('express');
+require('dotenv').config();
+
+
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
+console.log("clave", jwtSecret);
+console.log("puerto", port);
+
 
 app.get('/', (req, res) => {
     res.send('Api de backend funcionando');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
